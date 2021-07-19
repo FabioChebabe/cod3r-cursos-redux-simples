fb013205661a3f5aaaa3b1e8abc00a645c3aad43
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2021-07-19 15:25:33 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Intervalo from './components/Intervalo'
+import Media from './components/Media'
+import Soma from './components/Soma'
+import Sorteio from './components/Sorteio'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exercícios React-Redux(simples)</h1>
+      <div className='linha'> 
+        <Intervalo >X</Intervalo>
+      </div>
+      <div className='linha'> 
+        <Media></Media>
+        <Soma ></Soma>
+        <Sorteio></Sorteio>
+      </div>
+    </div>
+  );
+}
+
+export default App;
